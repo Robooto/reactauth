ecab7635533d8ea5aac1f2d751f06a13b9005e81
Sam Russell <samrussell63@gmail.com>
2017-01-19 20:13:17 -0800

    finshed up auth

---
+++ client/src/components/welcome.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+
+export default () => <div>Welcome To Earth!!</div>
--- client/src/index.js
+++ client/src/index.js
@@ -12,13 +12,23 @@ import Signup from './components/auth/signup';
 import reducers from './reducers';
 import Feature from './components/feature';
 import RequireAuth from './components/auth/require_auth';
+import Welcome from './components/welcome';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+// If we have a token, consider the user to be signed in
+if (token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
